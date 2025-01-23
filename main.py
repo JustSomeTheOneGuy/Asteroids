a24f76fdd86075a33216cc3d53b6f97ead468040
JustSomeTheOneGuy <ajphilli22@gmail.com>
2025-01-23 12:10:18 -0500

    added player and gave it the ability to move across the gamespace

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,7 +23,18 @@ def main():
                 return
             
         screen.fill("black")
+        for updates in updateable:
+            updates.update(dt)
+
+        for drawn in drawable:
+            drawn.draw(screen)
+
+        #player.draw(screen)
+        #player.update(dt)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
